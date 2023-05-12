539c0a7df7275cda74645d972c07afa3d0bcf5e6
liucuxiu <xliucuxiux@gmail.com>
2023-05-12 16:29:15 +0700

    test CI/CD: check lint

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -5,7 +5,7 @@ const UserService = require('./UserService');
 router.post('/api/v1/users', async (req, res) => {
   await UserService.saveUser(req.body);
 
-  return res.send({ message: 'User created' });
+  return res.send({ message: 'User created' })
 })
 
 module.exports = router;
